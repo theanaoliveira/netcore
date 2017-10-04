64835037a86b382bdeda93f223b2a0bf2487a86c
Ana Caroline <carolineana363@gmail.com>
2017-10-04 14:11:32 -0300

    Add files via upload

---
+++ InterfacesFacebook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace netcore
+{
+    interface IToken
+    {
+        string access_token { get; set; }
+        string token_type { get; set; }
+    }
+
+    interface ISearch
+    {
+        string id { get; set; }
+        string name { get; set; }
+        string about { get; set; }
+    }
+
+    interface IFacebookFeed
+    {
+        string message { get; set; }
+        string id { get; set; }
+    }
+}
