d4a1f843e3f77128a6c6e62390d0a96db7c19dfe
Ana Caroline <carolineana363@gmail.com>
2018-01-22 22:23:09 -0200

    Add files via upload

---
+++ GoogleApp/GoogleApp/GoogleModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleApp
+{
+    public class NewsModel
+    {
+        public string status { get; set; }
+        public int totalResults { get; set; }
+        public List<NewsArticle> articles { get; set; }
+    }
+
+    public class NewsArticle
+    {
+        public NewsSource source { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public string urlToImage { get; set; }
+        public string publishedAt { get; set; }
+    }
+
+    public class NewsSource
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ GoogleApp/GoogleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace GoogleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://newsapi.org/docs/endpoints/sources
+
+            //https://webhose.io/auth/signup
+
+            var lstrUrl = "https://newsapi.org/v2/everything?q=BRADESCO&apiKey=614a7a79524e4380bc8470638a3fb76b&pageSize=100";
+
+            var responseFromServer = ReadUrl(lstrUrl);
+            var json = JsonConvert.DeserializeObject<NewsModel>(responseFromServer);
+
+            //614a7a79524e4380bc8470638a3fb76b
+        }
+
+        /// <summary>
+        /// Le uma determinada URL e devolve o retorno
+        /// </summary>
+        /// <param name="pstrUrl">Url a ser lida</param>
+        /// <returns>string com o retorno da url</returns>
+        static string ReadUrl(string pstrUrl)
+        {
+            var url = pstrUrl;
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            var res = req.GetResponse();
+
+            // Get the stream containing content returned by the server.
+            var dataStream = res.GetResponseStream();
+
+            // Open the stream using a StreamReader for easy access.
+            var reader = new StreamReader(dataStream);
+
+            // Read the content.
+            var responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            res.Close();
+
+            return responseFromServer;
+        }
+    }
+}
